11d4d4da42cc49188d57b6a2961a94f204354950
laranja14 <laranja1419@gmail.com>
2022-03-31 18:51:00 +0900

    css 수정

--- pages/information.js
+++ pages/information.js
@@ -1,7 +1,9 @@
 export default function Information() {
     return (
         <div>
-            팀 정보
+            <div style={{ textAlign: "center", margin: "30px" }}>
+                <h2>팀 소개</h2>
+            </div>
         </div>
     )
 }
--- src/component/Betline.js
+++ src/component/Betline.js
@@ -1,18 +1,21 @@
+import styles from '../../styles/Betline.module.css';
+
 export default function Betline() {
     return (
-        <div style={{ display: 'flex', justifyContent: "center", margin: "5px 50px 5px 50px", border: "solid" }}>
-            <div style={{ border: "solid" }}>Champions Korea</div>
-            <div style={{ border: "solid" }}>승리</div>
-            <button style={{ border: "solid" }}>
-                <div>T1</div>
-                <div>1.26</div>
-            </button >
-            <div style={{ border: "solid" }}>VS</div>
-            <button style={{ border: "solid" }}>
-                <div>3.85</div>
-                <div>Gen.G</div>
-            </button>
-            <div style={{ border: "solid" }}>3월 3018:00 GMT+9</div>
+        <div className={styles.betlineWide}>
+            <div className={styles.tournament}>Champions Korea</div>
+            <duv className={styles.matchup}>
+                <button className={styles.leftMatch} >
+                    <div className={styles.leftTeam} >T1</div>
+                    <div className={styles.leftMagn} >1.26</div>
+                </button>
+                <div className={styles.middle}>VS</div>
+                <button className={styles.rightMatch}>
+                    <div className={styles.rightMagn}>3.85</div>
+                    <div className={styles.rightTeam}>Gen.G</div>
+                </button>
+            </duv>
+            <div className={styles.betlineDate}>3월 30일 18:00</div>
         </div>
     )
 }
